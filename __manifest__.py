843589ade9153d2cdd531e75aa71815df125a57a
cloe <cromsal087@g.educaand.es>
2025-02-11 09:03:17 +0100

    arreglado manifest para icono y referencia a garantias

--- __manifest__.py
+++ __manifest__.py
@@ -5,12 +5,12 @@
  'author': 'Cloe Romero',
  'category': 'Custom',
  'summary': 'Gestión de Mantenimiento de Productos',
- 'depends': ['base', 'garantia.producto'],
+ 'depends': ['base', 'garantias'],
  'data': [
  'security/ir.model.access.csv',
  'views/servicio_mantenimiento_views.xml',
  ],
- 'icon': '/static/description/icon55.png',
+ 'icon': '/servicio_mantenimiento/static/description/icon55.png',
  'installable': True,
  'application': True,
 }
